6724043e23ba81f01d0cba47d87268dd0393df27
Haven Ganesh <ganeshandimadam@gmail.com>
2024-06-17 22:02:00 +0530

    changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
