df51fe97b40c2b886951f175b260021af696c6e2
Haven Ganesh <ganeshandimadam@gmail.com>
2024-06-18 10:59:19 +0530

    property binding completed with img

--- src/app/property-binding/property-binding.component.ts
+++ src/app/property-binding/property-binding.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-property-binding',
   templateUrl: './property-binding.component.html',
-  styleUrls: ['./property-binding.component.css']
+  styleUrls: ['./property-binding.component.css'],
 })
 export class PropertyBindingComponent {
+  imgLocation = '../../assets/image/property_binding.png';
 
+  codeTemplate: string = `Code of above template
+   -------------------------------------------------------
+   HTML code
+   <div>
+    <img [src]="imgLocation" alt="item">
+   </div>
+   --------------------------------------------------------
+   Typescript code
+   imgLocation = '../../assets/image/property_binding.png';
+   `;
 }
--- src/app/string-interpolation/string-interpolation.component.ts
+++ src/app/string-interpolation/string-interpolation.component.ts
@@ -3,14 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-string-interpolation',
   templateUrl: './string-interpolation.component.html',
-  styleUrls: ['./string-interpolation.component.css']
+  styleUrls: ['./string-interpolation.component.css'],
 })
 export class StringInterpolationComponent {
+  title = 'Angular 16 practice';
+  user = {
+    name: 'angular user',
+    hobby: 'coding',
+    interest: 'UI',
+    known: ['java', 'go', 'javascript', 'typescript', 'html', 'css'],
+  };
+  codeTemplate: string = `Code of above template
+----------------------------------------------------------------
+HTML code 
+----------------------------------------------------------------
+  <div>
+    <h1>{{title}}</h1>
+    <h2>user : {{user.name}}</h2>
+    <h2>hobby : {{user.hobby}}</h2>
+    <h2>interest : {{user.interest}}</h2>
+    <h2>known : </h2>
+    <ol>
+        <li>{{user.known[0]}}</li>
+        <li>{{user.known[1]}}</li>
+        <li>{{user.known[2]}}</li>
+        <li>{{user.known[3]}}</li>
+    </ol>
+  </div>
+----------------------------------------------------------------
+Typescript Code
+----------------------------------------------------------------
   title = 'Angular 16 practice';
   user = {
     name: "angular user",
     hobby: "coding",
     interest: "UI",
     known: ["java", "go", "javascript", "typescript", "html", "css"]
-  }
+  }`;
 }
